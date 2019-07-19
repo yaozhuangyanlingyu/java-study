c462d35003610fca1585e552a1e245223a47c9ed
yaoxf <850630887@qq.com>
2019-07-19 18:17:08 +0800

    md

--- maven/maven_study_01/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
+++ maven/maven_study_01/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -8,20 +8,20 @@ public class AccountServiceImpl implements IAccountService {
      * 添加账户
      */
     public void addAccount() {
-
+        System.out.println("执行了 添加账户");
     }
 
     /**
      * 更新账户
      */
     public void updateAccount() {
-
+        System.out.println("执行了 更新账户");
     }
 
     /**
      * 删除账户
      */
     public void deleteAccount() {
-
+        System.out.println("执行了 删除账户");
     }
 }
