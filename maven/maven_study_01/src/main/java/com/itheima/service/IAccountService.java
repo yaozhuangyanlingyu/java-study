ad2513aff39c00c9d8222ba410aed4ab62d5e018
yaoxf <850630887@qq.com>
2019-07-19 18:05:17 +0800

    md

---
+++ maven/maven_study_01/src/main/java/com/itheima/service/IAccountService.java
@@ -0,0 +1,19 @@
+package com.itheima.service;
+
+public interface IAccountService {
+
+    /**
+     * 保存账户
+     */
+    void addAccount();
+
+    /**
+     * 修改账户
+     */
+    void updateAccount();
+
+    /**
+     * 删除账户
+     */
+    void deleteAccount();
+}
---
+++ maven/maven_study_01/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -0,0 +1,27 @@
+package com.itheima.service.impl;
+
+import com.itheima.service.IAccountService;
+
+public class AccountServiceImpl implements IAccountService {
+
+    /**
+     * 添加账户
+     */
+    public void addAccount() {
+
+    }
+
+    /**
+     * 更新账户
+     */
+    public void updateAccount() {
+
+    }
+
+    /**
+     * 删除账户
+     */
+    public void deleteAccount() {
+
+    }
+}
