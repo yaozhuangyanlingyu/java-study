9aaec41d534bf983edc2d26a5f28c60dff1e97be
yaoxf <850630887@qq.com>
2019-07-22 05:58:12 +0800

    md

--- servlet/servlet_study_03/src/cn/itcast/web/servlet/ServletDemo1.java
+++ servlet/servlet_study_03/src/cn/itcast/web/servlet/ServletDemo1.java
@@ -1,19 +1,17 @@
 package cn.itcast.web.servlet;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
 /**
- * Servlet快速入门
+ * 注解方式路由
  */
+@WebServlet(urlPatterns = "/demo")
 public class ServletDemo1 implements Servlet {
-    /**
-     * 初始化方法
-     * 在Servlet被创建时，才会执行一次
-     */
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
-        System.out.println("servlet init");
+
     }
 
     @Override
@@ -21,17 +19,9 @@ public class ServletDemo1 implements Servlet {
         return null;
     }
 
-    /**
-     * 提供服务的方法
-     * 每次servlet被访问时，执行
-     * @param servletRequest
-     * @param servletResponse
-     * @throws ServletException
-     * @throws IOException
-     */
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        System.out.println("hello servlet!");
+        System.out.println("hello servlet");
     }
 
     @Override
@@ -39,12 +29,8 @@ public class ServletDemo1 implements Servlet {
         return null;
     }
 
-    /**
-     * 销毁方法，在servlet被杀死时执行
-     * 在服务器正常关闭时执行
-     */
     @Override
     public void destroy() {
-        System.out.println("servlet destroy");
+
     }
 }
