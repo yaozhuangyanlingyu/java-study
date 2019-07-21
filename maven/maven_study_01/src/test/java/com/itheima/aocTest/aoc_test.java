0a2dc13c7760a21bd58d4321a063c4c225bd1488
yaoxf <850630887@qq.com>
2019-07-21 14:29:43 +0800

    md

---
+++ maven/maven_study_01/src/test/java/com/itheima/aocTest/aoc_test.java
@@ -0,0 +1,12 @@
+package com.itheima.aocTest;
+
+public class aoc_test {
+    public static void main(String[] args) {
+        // 获取容器
+        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+
+        // 获取对象
+
+        // 执行方法
+    }
+}
